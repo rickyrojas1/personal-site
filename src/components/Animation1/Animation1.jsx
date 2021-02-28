8ced38df7a10f4ecfe4433debcc25e6dcd97a3a7
rickyrojas <ricky.rojas@giantmachines.com>
2021-02-27 23:46:49 -0500

    use cryptos

--- src/components/Animation1/Animation1.jsx
+++ src/components/Animation1/Animation1.jsx
@@ -8,12 +8,12 @@ const Animation1 = () => (
       <div className="container__elements">
         <div className="pill"><span className="star star1" /><span className="star star2" /><span className="star star3" /><span className="star star4" /><span className="star star5" /><span className="star star6" /><span className="star star7" /><span className="star star8" /><span className="star star9" /><span className="star star10" /><span className="star star11" /><span className="star star12" /><span className="star star13" /><span className="star star14" /><span className="star star15" /><span className="star star16" /><span className="star star17" /><span className="star star18" /><span className="star star19" /><span className="star star20" /><span className="star star21" /><span className="star star22" /><span className="star star23" /><span className="star star24" /><img className="lanternHills" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/lanternHills.png" />
           <div className="lanternContainer">
-            <div className="lanternX1"><img className="lanternY1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
-            <div className="lanternX2"><img className="lanternY2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
-            <div className="lanternX3"><img className="lanternY3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
-            <div className="lanternX4"><img className="lanternY4" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
-            <div className="lanternX5"><img className="lanternY5" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
-            <div className="lanternX6"><img className="lanternY6" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3522775/LanternsLarge.png" /></div>
+            <div className="lanternX1"><img className="lanternY1" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpnommensen.com%2Fimages%2Fethereum-coin-png-8.png&f=1&nofb=1" /></div>
+            <div className="lanternX2"><img className="lanternY2" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Fbitcoin%2Fbitcoin_PNG37.png&f=1&nofb=1" /></div>
+            <div className="lanternX3"><img className="lanternY3" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Fbitcoin%2Fbitcoin_PNG37.png&f=1&nofb=1" /></div>
+            <div className="lanternX4"><img className="lanternY4" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2FaUSiZ12.png&f=1&nofb=1" /></div>
+            <div className="lanternX5"><img className="lanternY5" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpnommensen.com%2Fimages%2Fethereum-coin-png-8.png&f=1&nofb=1" /></div>
+            <div className="lanternX6"><img className="lanternY6" src=" https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.imgur.com%2FaUSiZ12.png&f=1&nofb=1 " /></div>
           </div>
         </div>
       </div>
